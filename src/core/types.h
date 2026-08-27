f805b9dd7c524e380b5ef5be0f96fc3225a5934b
Cole Godfrey <178968333+Cole-Godfrey@users.noreply.github.com>
2026-08-27 15:49:18 -0700

    Added more core types

--- src/core/types.h
+++ src/core/types.h
@@ -9,6 +9,9 @@
 namespace exchange::core {
 
     using Price = std::int64_t;
-
     using Quantity = std::uint32_t;
+    using OrderId = std::uint64_t;
+    using SequenceNumber = std::uint64_t;
+    using Timestamp = std::uint64_t;
+    using ParticipantId = std::uint32_t;
 }
