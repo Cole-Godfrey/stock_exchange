6df0e394fca2e9f4d4653c99f28d5352dff805d7
Cole Godfrey <178968333+Cole-Godfrey@users.noreply.github.com>
2026-08-29 15:41:38 -0700

    Added more core types

--- src/core/types.h
+++ src/core/types.h
@@ -14,4 +14,10 @@ namespace exchange::core {
     using SequenceNumber = std::uint64_t;
     using Timestamp = std::uint64_t;
     using ParticipantId = std::uint32_t;
+    using MatchId = std::uint64_t;
+    inline constexpr Price PRICE_SCALE = 10'000;
+    enum class Side : std::uint8_t {
+        BUY,
+        SELL
+    };
 }
