a5337d4e08befdfb1d2df540a620430a5a70d6bd
Cole Godfrey <178968333+Cole-Godfrey@users.noreply.github.com>
2026-08-31 16:24:56 -0700

    Added order types

--- src/core/types.h
+++ src/core/types.h
@@ -15,9 +15,23 @@ namespace exchange::core {
     using Timestamp = std::uint64_t;
     using ParticipantId = std::uint32_t;
     using MatchId = std::uint64_t;
+
     inline constexpr Price PRICE_SCALE = 10'000;
+
     enum class Side : std::uint8_t {
         BUY,
         SELL
+    };
+    enum class OrderType : std::uint8_t {
+        LIMIT,
+        MARKET,
+        IOC,
+        FOK,
+        GTC,
+        STOP,
+        STOP_LIMIT,
+        ICEBERG,
+        POST_ONLY,
+        
     };
 }
