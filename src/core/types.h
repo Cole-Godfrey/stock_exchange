760dc7fdca3cb70a4019c41a7386471d8eb8dcea
Cole Godfrey <178968333+Cole-Godfrey@users.noreply.github.com>
2026-09-02 16:32:26 -0700

    Added Symbol type + converters with string_view + lessthan operator

--- src/core/types.h
+++ src/core/types.h
@@ -41,4 +41,31 @@ namespace exchange::core {
         FILLED,
         CANCELED
     };
+
+    using Symbol = std::array<char, 8>;
+
+    constexpr Symbol make_symbol(std::string_view text) noexcept {
+        Symbol symbol{};
+
+        const auto length = std::min(symbol.size(), text.size());
+
+        for (std::size_t index = 0; index < length; ++index) {
+            symbol[index] = text[index];
+        }
+        return symbol;
+    }
+    constexpr std::string_view symbol_view(const Symbol &symbol) noexcept {
+        std::size_t length = 0;
+
+        while (length < symbol.size() && symbol[length] != '\0') {
+            ++length;
+        }
+        return std::string_view(symbol.data(), length);
+    }
+
+    struct SymbolLess {
+        constexpr bool operator()(const Symbol &lhs, const Symbol &rhs) const noexcept {
+            return std::lexicographical_compare(lhs.begin(), lhs.end(), rhs.begin(), rhs.end());
+        }
+    };
 }
