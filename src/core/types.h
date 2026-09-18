a9985c3e40bc9ed29f59cf38f3889fc99fc130b4
Cole Godfrey <178968333+Cole-Godfrey@users.noreply.github.com>
2026-09-18 16:11:43 -0700

    added static asserts to ensure data structures are trivially copyable.

--- src/core/types.h
+++ src/core/types.h
@@ -68,4 +68,16 @@ namespace exchange::core {
             return std::lexicographical_compare(lhs.begin(), lhs.end(), rhs.begin(), rhs.end());
         }
     };
+
+    static_assert(std::is_trivially_copyable_v<Price>);
+    static_assert(std::is_trivially_copyable_v<Quantity>);
+    static_assert(std::is_trivially_copyable_v<OrderId>);
+    static_assert(std::is_trivially_copyable_v<SequenceNumber>);
+    static_assert(std::is_trivially_copyable_v<Timestamp>);
+    static_assert(std::is_trivially_copyable_v<ParticipantId>);
+    static_assert(std::is_trivially_copyable_v<MatchId>);
+    static_assert(std::is_trivially_copyable_v<Side>);
+    static_assert(std::is_trivially_copyable_v<OrderType>);
+    static_assert(std::is_trivially_copyable_v<OrderStatus>);
+    static_assert(std::is_trivially_copyable_v<Symbol>);
 }
